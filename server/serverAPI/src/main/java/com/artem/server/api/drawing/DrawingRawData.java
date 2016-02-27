71ddfb3bdcae6f391ccd4bd237ef89d40ca811b9
Artem Spector <artem_spector@yahoo.com>
2016-02-27 13:11:53 +0200

    drawing data JSON serialization

--- server/serverAPI/src/main/java/com/artem/server/api/drawing/DrawingRawData.java
+++ server/serverAPI/src/main/java/com/artem/server/api/drawing/DrawingRawData.java
@@ -1,5 +1,8 @@
 package com.artem.server.api.drawing;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,12 +12,15 @@ import java.util.List;
  */
 public class DrawingRawData {
 
+    @JsonProperty("motions")
     private List<MotionData> motions = new ArrayList<>();
 
+    @JsonIgnore
     public synchronized void add(MotionData[] newMotions) {
         motions.addAll(Arrays.asList(newMotions));
     }
 
+    @JsonIgnore
     public synchronized MotionData[] getMotions(int startIdx) {
         int len = motions.size() - startIdx;
         if (len == 0) return new MotionData[0];
@@ -25,14 +31,17 @@ public class DrawingRawData {
         return res;
     }
 
+    @JsonIgnore
     public synchronized MotionData getLastMotion() {
         return motions.isEmpty() ? null : motions.get(motions.size() - 1);
     }
 
+    @JsonIgnore
     public void clear() {
         motions = new ArrayList<>();
     }
 
+    @JsonIgnore
     public boolean isEmpty() {
         return motions.isEmpty();
     }
--- server/serverAPI/src/main/java/com/artem/server/api/drawing/MotionData.java
+++ server/serverAPI/src/main/java/com/artem/server/api/drawing/MotionData.java
@@ -1,5 +1,8 @@
 package com.artem.server.api.drawing;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * created by artem on 1/23/16.
  */
@@ -7,11 +10,16 @@ public class MotionData {
 
     public enum MotionType {Up, Down, Move}
 
-    public final long time;
+    @JsonProperty("m")
     public final MotionType motion;
+    @JsonProperty("t")
+    public final long time;
+    @JsonProperty("x")
     public final float x;
+    @JsonProperty("y")
     public final float y;
 
+    @JsonIgnore
     public MotionData(long time, float x, float y, MotionType motion) {
         this.time = time;
         this.x = x;
@@ -20,7 +28,7 @@ public class MotionData {
     }
 
     public String toString() {
-        return "MotionData { motion=" + motion + " , x=" + x + ", y=" + y +" }";
+        return "{ motion=" + motion + " , x=" + x + ", y=" + y +" }";
     }
 
 }
