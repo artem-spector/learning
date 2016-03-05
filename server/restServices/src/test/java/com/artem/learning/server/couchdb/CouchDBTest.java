280fad2c96f43bdcaedf45d120d166183006f379
artems <artems@il.ibm.com>
2016-03-05 19:35:50 +0200

    cosmetics

--- server/restServices/src/test/java/com/artem/learning/server/couchdb/CouchDBTest.java
+++ server/restServices/src/test/java/com/artem/learning/server/couchdb/CouchDBTest.java
@@ -1,7 +1,6 @@
 package com.artem.learning.server.couchdb;
 
 import com.artem.learning.server.ServerApp;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
