0372814f13e8c2a422c14c7c84f2e92c6a431f10
artems <artems@il.ibm.com>
2016-03-16 18:23:02 +0200

    View supports simple or array keys

--- server/restServices/src/main/java/com/artem/learning/server/couchdb/View.java
+++ server/restServices/src/main/java/com/artem/learning/server/couchdb/View.java
@@ -1,5 +1,8 @@
 package com.artem.learning.server.couchdb;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * TODO: Document!
  *
@@ -12,6 +15,8 @@ public class View<ValueType> {
     private String viewName;
     private Class<ValueType> valueClass;
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     View(Database db, String designDocName, String viewName, Class<ValueType> valueClass) {
         this.db = db;
         this.designDocName = designDocName;
@@ -22,10 +27,10 @@ public class View<ValueType> {
     public LookupViewResponse<ValueType> lookup(Object startKey, Object endKey) {
         String url = db.dbUrl() + "/_design/" + designDocName + "/_view/" + viewName;
         String queryParam = "";
-        if (startKey != null) queryParam += "startkey=\"" + startKey + "\"";
+        if (startKey != null) queryParam += "startkey=" + asParameterValue(startKey);
         if (endKey != null) {
             if (startKey != null) queryParam += "&";
-            queryParam += "endkey=\"" + endKey + "\"";
+            queryParam += "endkey=" + asParameterValue(endKey);
         }
         if (!queryParam.isEmpty()) url += "?" + queryParam;
 
@@ -33,6 +38,17 @@ public class View<ValueType> {
         LookupViewResponse<ValueType> response = db.getRestTemplate().getForObject(url, LookupViewResponse.class);
         ViewRowValueDeserializer.clear();
         return response;
+    }
+
+    private String asParameterValue(Object obj) {
+        if (obj instanceof String)
+            return "\"" + obj + "\"";
+        else
+            try {
+                return mapper.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
     }
 
 }
--- server/restServices/src/main/java/com/artem/learning/server/dao/LessonDao.java
+++ server/restServices/src/main/java/com/artem/learning/server/dao/LessonDao.java
@@ -3,11 +3,13 @@ package com.artem.learning.server.dao;
 import com.artem.learning.server.couchdb.Database;
 import com.artem.learning.server.couchdb.LookupViewResponse;
 import com.artem.learning.server.couchdb.ViewRow;
+import com.artem.learning.server.model.DateTimeUtil;
 import com.artem.learning.server.model.Lesson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,7 +25,9 @@ public class LessonDao {
 
     public List<Lesson> getLessons(String studentId, String courseId) {
         List<Lesson> res = new ArrayList<>();
-        LookupViewResponse<Lesson> response = LessonDesignHelper.getLessonsByStudentAndCourseView(db).lookup(null, null);
+        String[] startKey = {studentId, courseId, DateTimeUtil.formatDateTime(new Date(0))};
+        String[] endKey = {studentId, courseId, DateTimeUtil.formatDateTime(new Date(System.currentTimeMillis() + 3600 * 1000))};
+        LookupViewResponse<Lesson> response = LessonDesignHelper.getLessonsByStudentAndCourseView(db).lookup(startKey, endKey);
         for (ViewRow<Lesson> row : response.getRows()) {
             res.add(row.getValue());
         }
