dae6671552b27db0b6c0e23b284af9f4f03422ef
artems <artems@il.ibm.com>
2016-03-29 11:03:06 +0300

    confirmation for delete student

--- server/restServices/src/main/resources/public/students.js
+++ server/restServices/src/main/resources/public/students.js
@@ -1,3 +1,8 @@
-function deleteStudent(id) {
-    alert("Delete student " + id);
+function deleteStudent(name, id) {
+    var doDelete = confirm("Are you sure to delete student " + name + "?");
+    if (doDelete) {
+        var deleteForm = document.getElementById('deleteStudentForm');
+        deleteForm.action = '/console/deleteStudent/' + id;
+        deleteForm.submit();
+    }
 }
